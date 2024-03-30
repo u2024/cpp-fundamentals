3d93c073702a90d28f43091dd5d0d5a6e920f412
u2024 <157313309+u2024@users.noreply.github.com>
2024-03-30 12:32:49 +0100

    Update nwdNww.hpp

--- homework/nwd-nnw/nwdNww.hpp
+++ homework/nwd-nnw/nwdNww.hpp
@@ -1,11 +1,36 @@
 #pragma once
 
 int NWD(int lhs, int rhs) {
-    // TODO: Implement me :)
-    return -1;
+        int r = 0;
+    if(lhs > rhs)
+    {
+    while(rhs != 0)
+    {
+        r = lhs % rhs;
+        lhs = rhs;
+        rhs = r;
+    }
+    }
+    else if(rhs > lhs)
+    {
+    while(lhs != 0)
+    {
+        r = rhs % lhs;
+        rhs = lhs;
+        lhs = r;
+    }
+    return std::abs(rhs);
+    }
+    else if(lhs == 0 && rhs == 0)
+    {
+        return 1;
+    }
+    return std::abs(lhs);
 }
 
 int NWW(int lhs, int rhs) {
-    // TODO: Implement me :)
-    return -1;
+    if(lhs == 0 || rhs == 0) {
+        return 0;
+    }
+    else return std::abs((lhs * rhs) / GCD(lhs, rhs));
 }
