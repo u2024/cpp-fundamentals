291a1490be5e15a84017ae2c4e5433f7b34d186f
u2024 <157313309+u2024@users.noreply.github.com>
2024-03-31 16:51:59 +0200

    Update maxOfVector.hpp

--- homework/max-of-vector/maxOfVector.hpp
+++ homework/max-of-vector/maxOfVector.hpp
@@ -3,6 +3,11 @@
 #include <vector>
 
 int maxOfVector(const std::vector<int>& vec) {
-    // TODO: Implement me :)
-    return {};
+    int index_max = 0;
+    for(int i = 1 ; i < vec.capacity() ; ++i) {
+        if(vec[index_max] < vec[i]) {
+            index_max = i;
+        }
+    }
+    return vec[index_max];
 }
