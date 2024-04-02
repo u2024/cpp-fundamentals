802c65198363de12aa915a497973a6694ae0e27a
u2024 <157313309+u2024@users.noreply.github.com>
2024-04-02 17:22:39 +0200

    Update validation.hpp

--- homework/password-check/validation.hpp
+++ homework/password-check/validation.hpp
@@ -12,4 +12,4 @@ enum ErrorCode
 auto getErrorMessage(ErrorCode result);
 bool doPasswordsMatch(std::string password_1, std::string password_2);
 auto checkPasswordRules(std::string password);
-auto checkPassword(std::string password_1, std::string password_2);
+ErrorCode checkPassword(std::string password_1, std::string password_2);
