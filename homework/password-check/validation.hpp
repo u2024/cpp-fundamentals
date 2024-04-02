c014453da8e9d1a6f681571791da52a1d12207ec
u2024 <157313309+u2024@users.noreply.github.com>
2024-04-02 17:38:23 +0200

    Update validation.hpp

--- homework/password-check/validation.hpp
+++ homework/password-check/validation.hpp
@@ -1,5 +1,4 @@
 #pragma once
-#include <string>
 enum ErrorCode
 {
     Ok,
@@ -9,7 +8,7 @@ enum ErrorCode
     PasswordNeedsAtLeastOneUppercaseLetter,
     PasswordsDoNotMatch,
 };
-string getErrorMessage(ErrorCode result);
+auto getErrorMessage(ErrorCode result);
 bool doPasswordsMatch(std::string password_1, std::string password_2);
 ErrorCode checkPasswordRules(std::string password);
 ErrorCode checkPassword(std::string password_1, std::string password_2);
